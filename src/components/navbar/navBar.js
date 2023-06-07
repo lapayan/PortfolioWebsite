51555832da932edea81a0cd03c9611d0d135124a
bigsexy <luispayan2001@gmail.com>
2023-06-07 10:56:04 -0600

    Completed the navigation bar whichb is just the logo and name

--- src/components/navbar/navBar.js
+++ src/components/navbar/navBar.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import {ReactComponent as Panda} from '../../logo/personalBrandLogo.svg'; 
 import './navBar.css';
-const name = "<Luis Payan/>";
 const NavBar = () => {
     return (
         <div className = 'mainNavBar'> 
            <a className = 'homeLink' href = '#'>  
              <Panda className = "logo" /> 
            </a>
-                <h2 className='name'> {name} </h2> 
+                <h2 className='name'> {`<Luis Payan/>`} </h2> 
         </div> 
     )
 }; 
