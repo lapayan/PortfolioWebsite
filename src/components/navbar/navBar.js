4e20925f4260caaf51a7576080af95ce282e53f1
bigsexy <luispayan2001@gmail.com>
2023-06-08 10:14:51 -0600

    Navigation Bar Finished

--- src/components/navbar/navBar.js
+++ src/components/navbar/navBar.js
@@ -1,15 +1,53 @@
-import React from 'react';
-import {ReactComponent as Panda} from '../../logo/personalBrandLogo.svg'; 
+import React, {useState, useEffect } from 'react';
+import TypeWriterEffect from 'react-typewriter-effect';
+import {ReactComponent as Github} from '../../assets/github.svg'; 
+import {ReactComponent as LinkedIn} from '../../assets/linkedIn.svg'; 
+import {ReactComponent as Instagram} from '../../assets/instagram.svg'; 
+
 import './navBar.css';
+
+
 const NavBar = () => {
+    const [text, setText] = useState('');
+    const [fullText, setfullText] = useState('<Luis Payan/>');
+    const [index,setIndex] = useState(0);
+    //
+    
+    useEffect(() => {
+        if (index < fullText.length) {
+          setTimeout(() => {
+            setText(text + fullText[index])
+            setIndex(index + 1)
+          }, 400)
+        }
+      }, [index]);
+
+    //
     return (
         <div className = 'mainNavBar'> 
-           <a className = 'homeLink' href = '#'>  
-             <Panda className = "logo" /> 
-           </a>
-                <h2 className='name'> {`<Luis Payan/>`} </h2> 
+           <div className = 'name'> 
+                <TypeWriterEffect
+                startDelay={100}
+                cursorColor="black"
+                text='{name: Luis Payan}'
+                typeSpeed={80}
+                eraseSpeed={100}
+                />
+            </div>
+            <div className = 'socialsContainer'> 
+                <a href = 'https://github.com/lapayan'>
+                    <Github className='github' />
+                </a>
+                <a href = 'https://www.linkedin.com/in/luis-payan-6814b1243/'>
+                    <LinkedIn className='linkedIn'/>
+                </a>
+                <a href ='https://www.instagram.com/pikapi293/'>
+                    <Instagram className='instagram'/>
+                </a>
+            </div>
         </div> 
     )
+    //
 }; 
 
 export default NavBar;
